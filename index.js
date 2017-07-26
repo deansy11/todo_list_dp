70d21815256077afdf84d1769d15c27956c15f1f
Emily Deans <emilyldeans@gmail.com>
2017-07-26 15:48:23 -0400

    Initial Commit.

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mustacheExpress = require("mustache-express");
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "mustache")
+
+// app.use(express.static("routefile???"))
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(8080, () => {
+  console.log("Node running successfully at http://localhost:3000");
+})
